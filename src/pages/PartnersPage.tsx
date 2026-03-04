723b262ecdf96025a3dffeada22a180568a53ccb
chinedu Umukoro <umukorochinedu@gmail.com>
2026-03-04 05:41:23 +0100

    added modification to project

--- src/pages/PartnersPage.tsx
+++ src/pages/PartnersPage.tsx
@@ -22,7 +22,7 @@ export function PartnersPage() {
         <CardContent className="p-0">
           <div className="grid lg:grid-cols-2">
             {/* Left Side - Visual Content */}
-            <div className="bg-gradient-to-br from-black  to-gray-300 p-8 sm:p-12 text-white relative overflow-hidden">
+            <div className="bg-black p-8 sm:p-12 text-white relative overflow-hidden">
               {/* Animated Background Pattern */}
               <div className="absolute inset-0 opacity-15">
                 <div className="absolute top-0 left-0 w-40 h-40 bg-white rounded-full blur-2xl animate-pulse"></div>
